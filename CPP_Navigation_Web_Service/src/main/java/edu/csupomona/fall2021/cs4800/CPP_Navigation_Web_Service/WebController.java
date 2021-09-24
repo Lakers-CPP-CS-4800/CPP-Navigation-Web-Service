0ac6bf3eee51151a0a163f2d520c8cf9e4f7eeb0
ZohnnyInc <zohnnyinc@gmail.com>
2021-09-23 20:52:37 -0700

    Update WebController.java

--- CPP_Navigation_Web_Service/src/main/java/edu/csupomona/fall2021/cs4800/CPP_Navigation_Web_Service/WebController.java
+++ CPP_Navigation_Web_Service/src/main/java/edu/csupomona/fall2021/cs4800/CPP_Navigation_Web_Service/WebController.java
@@ -2,6 +2,7 @@ package edu.csupomona.fall2021.cs4800.CPP_Navigation_Web_Service;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.math3.util.FastMath;
+import org.apache.commons.text.translate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,5 +50,12 @@ public class WebController {
 	@GetMapping("/jordantbui/guava")
 	public boolean compareNull() {
 		return Objects.equal(null, "a"); // returns false
+	}
+	
+	@GetMapping("/congzeng/translate")
+	public void whenTranslate_thenCorrect() {
+		UnicodeEscaper ue = UnicodeEscaper.above(0);
+		String result = ue.translate("ABCD");
+    		assertEquals("\\u0041\\u0042\\u0043\\u0044", result);
 	}
 }
