aab325283f4c698cb0a95327626147db16c7ef59
PaytonPerchez <49881436+PaytonPerchez@users.noreply.github.com>
2021-11-21 00:45:37 -0800

    Implemented temporary controls for course searching

--- CPP_Navigation_Web_Service/src/main/resources/static/js/app.js
+++ CPP_Navigation_Web_Service/src/main/resources/static/js/app.js
@@ -2,6 +2,8 @@ $( document ).ready(init());
 
 var nav_bar_open;
 var classes;
+var directionsService;
+var directionsRenderer;
 
 //make sure nothing triggers before the page is ready
 function init(){
@@ -12,7 +14,7 @@ function init(){
 	});
 	
 	//load in the map
-	loadMap();
+	//loadMap();
 	adjustMapSize();
 	readSavedClasses();
 	/*if(window.innerWidth >= 800){
@@ -31,25 +33,84 @@ function toggleBar(){
 	nav_bar_open = !nav_bar_open;
 }
 
-function loadMap(){
-	$.ajax({
-        type: "POST",
-        contentType: "application/json",
-		url: "/js/getMap",
-		success: function (data){
-			$("#map").html(data);
-		}
-	});
+//function loadMap(){
+//$.ajax({
+//    type: "POST",
+//    contentType: "application/json",
+//	url: "/js/getMap",
+//	success: function (data){
+//		$("#map").html(data);
+//	}
+//});
+//}
+
+// Initializes google map
+function initMap(){
+	
+	directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer();
+    const cpp = { lat: 34.058881, lng: -117.819725 };
+    // The map, centered at CPP
+    var map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 12,
+      center: cpp,
+    });
+    directionsRenderer.setMap(map);
 }
 
-function getInfo() {
+// Calculates route between start and end
+function calcRoute() {
+	/*
+	var dest1 = document.getElementById('start').value	// Get start building (Ex: "Building 8")
+	var dest2 = document.getElementById('end').value;	// Get end building (Ex: "Building 1")
 	
+	let startIndex = getIndex(dest1);	// Get the index of the start building in the location array
+	let endIndex = getIndex(dest2);		// Get the index of the end building in the location array
+	
+	// Ensure the indices are valid
+	if((startIndex >= 0) && (endIndex >= 0) && (startIndex != endIndex)) {
+	
+		var start = { lat: locations[startIndex][1], lng: locations[startIndex][2] } // lat and lng of start building
+		var end = { lat: locations[endIndex][1], lng: locations[endIndex][2] }		 // lat and lng of end building
+		
+		var request = {
+			origin: start,
+			destination: end,
+			travelMode: 'WALKING'
+		};
+		
+		// API call
+		directionsService.route(request, function(result, status) {
+			if (status == 'OK') {
+				directionsRenderer.setDirections(result);
+			}
+		});
+	}
+	*/
+}
+
+// Returns the index of the specified building
+function getIndex(string) {
+	  /*
+	  for(let i = 0; i < locations.length; i++) {
+		  if(locations[i][0].includes(string)) {
+			  return i;
+		  }
+	  }
+	  return -1;
+	  */
+}
+
+// Temporary function for backend testing
+function getInfo() {
+	//console.log(document.getElementById("subject").value + ", " + document.getElementById("num").value);
 	$.ajax({
         type: "GET",
         contentType: "application/json",
-		url: "/sections/" + $("#subject").value + "/" + $("#num").value,
+		url: "/sections/" + document.getElementById("subject").value + "/" + document.getElementById("num").value,
 		success: function (data){
-			$("#map").html(data);
+			$("#jsonTest").text(data);
+			console.log(data);
 		}
 	});
 }
